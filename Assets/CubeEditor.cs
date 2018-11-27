5113c068dca9690b16dee6dac9f750237c9fa73d
KuKKilicious <clouseyda@yahoo.de>
2018-11-27 19:52:11 +0100

    added CubeEditorScript and Debug Coordinates

--- Assets/CubeEditor.cs
+++ Assets/CubeEditor.cs
@@ -2,16 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 [ExecuteInEditMode]
-public class EditorSnap : MonoBehaviour {
+[SelectionBase]
+public class CubeEditor : MonoBehaviour {
 
     [SerializeField][Range(1f,20f)]
     float gridSize = 10f;
-	// Update is called once per frame
-	void Update () {
+
+    TextMesh textMesh;
+    
+    // Update is called once per frame
+    void Update () {
+      
+
         Vector3 snapPos;
         snapPos.x = Mathf.RoundToInt(transform.position.x / gridSize) * gridSize;
         snapPos.y = 0f;
         snapPos.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;
         transform.position = snapPos;
+        textMesh = GetComponentInChildren<TextMesh>();
+        textMesh.text = "("+snapPos.x/gridSize+","+snapPos.z/gridSize+")";
     }
+
+
 }
