a4a9e11b90c0897594e47f3181877196e5d1784f
KuKKilicious <clouseyda@yahoo.de>
2018-12-01 21:37:55 +0100

    implemented Enemy Spawning

--- Assets/EnemySpawner.cs
+++ Assets/EnemySpawner.cs
@@ -1,16 +1,30 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
+    [SerializeField]
+    float secendsBetweenSpawns = 2f;
+    [SerializeField]
+    EnemyMovement enemyToSpawn;
+    [SerializeField]
+    int amountOfEnemiesToSpawn = 12;
+    // Use this for initialization
+    void Start () {
+        StartCoroutine(SpawnEnemyEachSeconds(secendsBetweenSpawns));
 	}
-	
-	// Update is called once per frame
-	void Update () {
+
+    private IEnumerator SpawnEnemyEachSeconds(float secendsBetweenSpawns) {
+
+        for (int i = 0; i < amountOfEnemiesToSpawn; i++) {
+            Instantiate(enemyToSpawn,transform.position,Quaternion.identity,transform);
+            yield return new WaitForSeconds(secendsBetweenSpawns); 
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
 		
 	}
 }
--- Assets/Tower.cs
+++ Assets/Tower.cs
@@ -4,20 +4,48 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Tower : MonoBehaviour {
+
+    //Parameters of each tower
     [SerializeField]
     Transform objectToPan;
     [SerializeField]
-    Transform targetEnemy;
-    [SerializeField]
     float towerRange = 31f;
 
+    //State of each tower
+    Transform targetEnemy;
     // Update is called once per frame
     void Update() {
-        objectToPan.LookAt(targetEnemy);
+        SetTargetEnemy();
 
+        objectToPan.LookAt(targetEnemy);
         EnableEmission(enemyInRange());
 
 
+    }
+
+    private void SetTargetEnemy() {
+        var sceneEnemies = FindObjectsOfType<EnemyHealthHandler>();
+        if (sceneEnemies.Length <= 0) {
+            return;
+        }
+
+        Transform closestEnemy = sceneEnemies[0].transform;
+
+        foreach (EnemyHealthHandler testEnemy in sceneEnemies) {
+            closestEnemy = GetClosest(closestEnemy, testEnemy.transform);
+        }
+        targetEnemy = closestEnemy;
+    }
+
+    private Transform GetClosest(Transform closestEnemy, Transform testEnemy) {
+        float distToClosestEnemy1 = Vector3.Distance(gameObject.transform.position, closestEnemy.position);
+        float distToClosestEnemy2 = Vector3.Distance(gameObject.transform.position, testEnemy.position);
+
+        if(distToClosestEnemy1 < distToClosestEnemy2) {
+            return closestEnemy;
+        }else {
+            return testEnemy;
+        }
     }
 
     private bool enemyInRange() {
